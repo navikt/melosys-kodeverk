f84c4a7e39f5095a9b76ae864bae48e3461c357d
JohanSakshaug <johan.sakshaug@nav.no>
2019-01-21 13:00:07 +0100

    Legg til sokkelbegrunnelser

---
+++ src/begrunnelser/sokkelBegrunnelser.js
@@ -0,0 +1,24 @@
+/**
+ * Kodeverk/vesentligVirksomhet
+ * ref: https://confluence.adeo.no/display/TEESSI/Kodeverk+i+Melosys
+ * @module
+ */
+const sokkelBegrunnelser = [
+  {
+    kode: 'IKKE_EGEN_FREMDRIFT',
+    term: 'Ikke framdrift for egen maskin'
+  },
+  {
+    kode: 'IKKE_ORDINAER_SKIPSFART',
+    term: 'Skrogtypen kan ikke brukes i ordinær skipsfart'
+  },
+  {
+    kode: 'BORESKIP',
+    term: 'Boreskip - Sokkel'
+  },
+  {
+    kode: 'FAST_INSTALLASJON',
+    term: 'Fast installasjon'
+  }
+];
+module.exports.sokkelBegrunnelser = sokkelBegrunnelser;
