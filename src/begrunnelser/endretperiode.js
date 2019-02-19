26f56f6dbef9439c8695bc99f2a2acfe19809930
JohanSakshaug <46343153+JohanSakshaug@users.noreply.github.com>
2019-02-19 09:04:29 +0100

    Legg til endret periode begrunnelser (#28)

---
+++ src/begrunnelser/endretperiode.js
@@ -0,0 +1,16 @@
+const endretperiode = [
+  {
+    kode: 'RETURNERT_NORGE',
+    term: 'Arbeidstakeren har returnert til Norge',
+  },
+  {
+    kode: 'ARBEIDSFORHOLD_AVSLUTTET',
+    term: 'Arbeidsforholdet med utsendende arbeidsgiver er avsluttet',
+  },
+  {
+    kode: 'ENDRINGER_ARBEIDSSITUASJON',
+    term: 'Endringer i arbeidssituasjonen',
+  }
+];
+
+module.exports.endretperiode = endretperiode;
