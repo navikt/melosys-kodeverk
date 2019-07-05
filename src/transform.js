b705289a94ba83d80e6e52a08aa6aec3051044fc
Are Bjolseth <abjolseth@gmail.com>
2019-07-05 14:17:23 +0200

    Refactored code to avoid duplicates, applied arrayReducerFunc, which is different between map and set.

--- src/transform.js
+++ src/transform.js
@@ -1,16 +1,23 @@
 const arrayToObjectSet = (array) => {
   return array.reduce((obj, item) => {
-    obj[item.kode] = item.kode;
+    obj[item.kode] = item.kode;  // NB! kode => kode
     return obj;
   }, {});
 };
 
-const kodeverk2KodeSet = kodeverk => {
+const arrayToObjectMap = (array) => {
+  return array.reduce((obj, item) => {
+    obj[item.kode] = item.term; // NB! kode => term
+    return obj;
+  }, {});
+};
+
+const kodeverk2Kode = (kodeverk, arrayReducerFunc) => {
   let codes = {};
   for (const verk in kodeverk) {
     const node = kodeverk[verk];
     if (Array.isArray(node)) {
-      const obj = arrayToObjectSet(node);
+      const obj = arrayReducerFunc(node);
       codes[verk] = { ...obj };
     }
     else {
@@ -19,57 +26,22 @@ const kodeverk2KodeSet = kodeverk => {
         const obj = {};
         let node_prop = node[prop];
         if (Array.isArray(node_prop)) {
-          obj[prop] = {...arrayToObjectSet(node_prop)};
-          codes[verk] = {...codes[verk], ...obj};
+          obj[prop] = {...arrayReducerFunc(node_prop)};
         }
         else {
           obj[prop] = {};
           for (const np in node_prop) {
             obj[prop][np] = {...arrayToObjectSet(node_prop[np])};
-            codes[verk] = {...codes[verk], ...obj};
           }
         }
+        codes[verk] = {...codes[verk], ...obj};
       }
     }
   }
   return codes;
 };
+const kodeverk2KodeSet = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectSet);
 module.exports.kodeverk2KodeSet = kodeverk2KodeSet;
 
-const arrayToObjectMap = (array) => {
-  return array.reduce((obj, item) => {
-    obj[item.kode] = item.term;
-    return obj;
-  }, {});
-};
-
-const kodeverk2KodeMap = kodeverk => {
-  let codes = {};
-  for (const verk in kodeverk) {
-    const node = kodeverk[verk];
-    if (Array.isArray(node)) {
-      const obj = arrayToObjectMap(node);
-      codes[verk] = { ...obj };
-    }
-    else {
-      codes[verk] = {};
-      for(const prop in node) {
-        const obj = {};
-        let node_prop = node[prop];
-        if (Array.isArray(node_prop)) {
-          obj[prop] = {...arrayToObjectMap(node[prop])};
-          codes[verk] = {...codes[verk], ...obj};
-        }
-        else {
-          obj[prop] = {};
-          for (const np in node_prop) {
-            obj[prop][np] = {...arrayToObjectMap(node_prop[np])};
-            codes[verk] = {...codes[verk], ...obj};
-          }
-        }
-      }
-    }
-  }
-  return codes;
-};
+const kodeverk2KodeMap = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectMap);
 module.exports.kodeverk2KodeMap = kodeverk2KodeMap;
