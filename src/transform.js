978057a6873862d0b0cfa754661256e8cffabe6e
Bjølseth, Are <are.bjolseth@nav.no>
2019-07-08 15:35:39 +0200

    Mini adjustment and setting level depth with a constant.

--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,4 @@
+const MAX_LEVELS = 5;
 const arrayToObjectSet = (array) => {
   return array.reduce((obj, item) => {
     obj[item.kode] = item.kode;  // NB! kode => kode
@@ -12,27 +13,28 @@ const arrayToObjectMap = (array) => {
   }, {});
 };
 
-const kodeverk2Kode = (kodeverk, counter, arrayReducerFunc) => {
-  if (counter > 5) {
+const kodeverk2Kode = (kodeverk, arrayReducerFunc, counter = 1) => {
+  if (counter > MAX_LEVELS) {
+    console.error(`Max levels = ${MAX_LEVELS} Reached`);
     return {};
   }
-  
+
   if (Array.isArray(kodeverk)) {
     return arrayReducerFunc(kodeverk);
   }
-  
+
   let codes = {};
   for (const verk in kodeverk) {
     const node = kodeverk[verk];
-    codes[verk] = kodeverk2Kode(node, counter + 1, arrayReducerFunc);
+    codes[verk] = kodeverk2Kode(node,  arrayReducerFunc, counter+1);
   }
   return codes;
 };
 
 
 
-const kodeverk2KodeSet = kodeverk => kodeverk2Kode(kodeverk, 1, arrayToObjectSet);
+const kodeverk2KodeSet = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectSet);
 module.exports.kodeverk2KodeSet = kodeverk2KodeSet;
 
-const kodeverk2KodeMap = kodeverk => kodeverk2Kode(kodeverk, 1, arrayToObjectMap);
+const kodeverk2KodeMap = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectMap);
 module.exports.kodeverk2KodeMap = kodeverk2KodeMap;
