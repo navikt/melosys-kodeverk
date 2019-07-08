69c6fd1d027eb4065afffedcd2bd90489cfcf2c7
Wålberg, Knut Bjørnar <Knut.Bjornar.Walberg@nav.no>
2019-07-08 15:11:38 +0200

    Rekursiv generering av kodeverksstruktur

--- src/transform.js
+++ src/transform.js
@@ -12,36 +12,27 @@ const arrayToObjectMap = (array) => {
   }, {});
 };
 
-const kodeverk2Kode = (kodeverk, arrayReducerFunc) => {
+const kodeverk2Kode = (kodeverk, counter, arrayReducerFunc) => {
+  if (counter > 5) {
+    return {};
+  }
+  
+  if (Array.isArray(kodeverk)) {
+    return arrayReducerFunc(kodeverk);
+  }
+  
   let codes = {};
   for (const verk in kodeverk) {
     const node = kodeverk[verk];
-    if (Array.isArray(node)) {
-      const obj = arrayReducerFunc(node);
-      codes[verk] = { ...obj };
-    }
-    else {
-      codes[verk] = {};
-      for(const prop in node) {
-        const obj = {};
-        let node_prop = node[prop];
-        if (Array.isArray(node_prop)) {
-          obj[prop] = {...arrayReducerFunc(node_prop)};
-        }
-        else {
-          obj[prop] = {};
-          for (const np in node_prop) {
-            obj[prop][np] = {...arrayReducerFunc(node_prop[np])};
-          }
-        }
-        codes[verk] = {...codes[verk], ...obj};
-      }
-    }
+    codes[verk] = kodeverk2Kode(node, counter + 1, arrayReducerFunc);
   }
   return codes;
 };
-const kodeverk2KodeSet = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectSet);
+
+
+
+const kodeverk2KodeSet = kodeverk => kodeverk2Kode(kodeverk, 1, arrayToObjectSet);
 module.exports.kodeverk2KodeSet = kodeverk2KodeSet;
 
-const kodeverk2KodeMap = kodeverk => kodeverk2Kode(kodeverk, arrayToObjectMap);
+const kodeverk2KodeMap = kodeverk => kodeverk2Kode(kodeverk, 1, arrayToObjectMap);
 module.exports.kodeverk2KodeMap = kodeverk2KodeMap;
