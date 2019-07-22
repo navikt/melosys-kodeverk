6ac761ffe55e6f712ab5df2016f57353fd65e2dd
Bjølseth, Are <are.bjolseth@nav.no>
2019-07-22 16:00:01 +0200

    Tweeking && debugging

--- scripts/util.js
+++ scripts/util.js
@@ -13,6 +13,7 @@ const writeYmlFileSync = (navn, ymlText) => {
 const writeSemverText = semver => {
   createDistDir();
   const buildno = process.env.CIRCLE_BUILD_NUM;
+  console.log('process.env', process.env);
   const version = buildno.length > 0 ? `${semver}\n` :`${semver}:${buildno}\n`;
   fs.writeFileSync(`${DIST_DIR}/semver.txt`, `${version}\n`);
 };
