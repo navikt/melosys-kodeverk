768897afbf005007d6bf16ed41e5f63d832fdb53
Knut Bjørnar Wålberg <Knut.Bjornar.Walberg@nav.no>
2019-08-16 13:00:17 +0200

    MELOSYS-2897 - Oppdatert enkelte kodeverkstyper til mer forklarende navn

--- src/lovvalgsbestemmelser/index.js
+++ src/lovvalgsbestemmelser/index.js
@@ -1,10 +1,10 @@
-const { forordning_883_2004 } = require('./forordning_883_2004');
-const { forordning_987_2009 } = require('./forordning_987_2009');
-const { tillegg } = require('./tillegg');
+const { lovvalgbestemmelser_883_2004 } = require('./lovvalgbestemmelser_883_2004');
+const { lovvalgbestemmelser_987_2009 } = require('./lovvalgbestemmelser_987_2009');
+const { tilleggsbestemmelser_883_2004 } = require('./tilleggsbestemmelser_883_2004');
 
 const lovvalgsbestemmelser = {
-  forordning_883_2004,
-  forordning_987_2009,
-  tillegg,
+  lovvalgbestemmelser_883_2004,
+  lovvalgbestemmelser_987_2009,
+  tilleggsbestemmelser_883_2004,
 };
 module.exports.lovvalgsbestemmelser = lovvalgsbestemmelser;
