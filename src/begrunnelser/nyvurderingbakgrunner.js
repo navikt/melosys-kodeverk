bd5c642dfac724fe9ba8b6a50256297caf3dcb23
JohanSakshaug <johan.sakshauger@nav.no>
2019-11-19 14:07:20 +0100

    Legg til nyvurderingbakgrunner

---
+++ src/begrunnelser/nyvurderingbakgrunner.js
@@ -0,0 +1,21 @@
+/**
+ * Kodeverk/nyvurderingbakgrunner
+ * ref: https://confluence.adeo.no/display/TEESSI/Kodeverk+i+Melosys
+ * @module
+ */
+const nyvurderingbakgrunner = [
+  {
+    kode: 'SAKSBEHANDLER_OPPDAGET_FEIL',
+    term: 'Saksbehandler oppdaget feil'
+  },
+  {
+    kode: 'HENVENDELSE_OM_OPPDAGET_FEIL',
+    term: 'Mottatt henvendelse om oppdaget feil'
+  },
+  {
+    kode: 'HENVENDELSE_OM_FEIL_BRUKER',
+    term: 'Bruker har meldt fra om feil'
+  }
+];
+
+module.exports.nyvurderingbakgrunner = nyvurderingbakgrunner;
