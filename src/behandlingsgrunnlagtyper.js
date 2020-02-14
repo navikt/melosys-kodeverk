e4c22d2929934751aa208d42d1ed75dd08fa2740
Andreas Strand <andreas.strand@nav.no>
2020-02-14 09:35:35 +0100

    Ny struktur behandlingsgrunnlagtyper. Definerer hvilke opplysninger som er oppgitt i behandligsgrunnlaget

---
+++ src/behandlingsgrunnlagtyper.js
@@ -0,0 +1,16 @@
+/**
+ * Kodeverk/BehandlingsgrunnlagTyper
+ * @module
+ */
+const behandlingsgrunnlagtyper = [
+  {
+    kode: 'GENERELT',
+    term: 'Generelle opplysninger',
+  },
+  {
+    kode: 'SØKNAD',
+    term: 'Opplysninger fra søknad',
+  },
+];
+
+module.exports.behandlingsgrunnlagtyper = behandlingsgrunnlagtyper;
