24b3c9306e9365e36690c3b83d8bb68c62f58d44
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2020-03-04 12:50:16 +0100

    Legger til overgangsregelbestemmelser

---
+++ src/lovvalgsbestemmelser/overgangsregelbestemmelser.js
@@ -0,0 +1,23 @@
+const overgangsregelbestemmelser = [
+  {
+    kode: 'FO_1408_1971_ART14_2_A',
+    term: 'Rfo. 1408/1971 artikkel 14 nr. 2 bokstav a',
+  },
+  {
+    kode: 'FO_1408_1971_ART14_2_B',
+    term: 'Rfo. 1408/1971 artikkel 14 nr. 2 bokstav b',
+  },
+  {
+    kode: 'FO_1408_1971_ART14A_2',
+    term: 'Rfo. 1408/1971 artikkel 14a nr. 2',
+  },
+  {
+    kode: 'FO_1408_1971_ART14C_A',
+    term: 'Rfo. 1408/1971 artikkel 14c bokstav a',
+  },
+  {
+    kode: 'FO_1408_1971_ART14C_B',
+    term: 'Rfo. 1408/1971 artikkel 14c bokstav b',
+  },
+];
+module.exports.overgangsregelbestemmelser = overgangsregelbestemmelser;
