99f09767f01fc59b7d5c6a89b5e52d907e18f09d
JohanSakshaug <johan.sakshauger@nav.no>
2020-05-25 11:19:11 +0200

    Fjern Rfo. 883/2004 fra de fleste lovvalgsbestemmelsetekster

--- src/lovvalgsbestemmelser/tilleggsbestemmelser_883_2004.js
+++ src/lovvalgsbestemmelser/tilleggsbestemmelser_883_2004.js
@@ -2,23 +2,23 @@
 const tilleggsbestemmelser_883_2004 = [
   {
     kode: 'FO_883_2004_ART11_2',
-    term: 'Rfo. 883/2004 artikkel 11 nr. 2',
+    term: 'artikkel 11 nr. 2',
   },
   {
     kode: 'FO_883_2004_ART11_4_1',
-    term: 'Rfo. 883/2004 artikkel 11 nr. 4 første punktum',
+    term: 'artikkel 11 nr. 4 første punktum',
   },
   {
     kode: 'FO_883_2004_ART11_5',
-    term: 'Rfo. 883/2004 artikkel 11 nr. 5',
+    term: 'artikkel 11 nr. 5',
   },
   {
     kode: 'FO_883_2004_ART87_8',
-    term: 'Rfo. 883/2004 artikkel 87 nr. 8',
+    term: 'artikkel 87 nr. 8',
   },
   {
     kode: 'FO_883_2004_ART87A',
-    term: 'Rfo. 883/2004 artikkel 87a',
+    term: 'artikkel 87a',
   }
 ];
 module.exports.tilleggsbestemmelser_883_2004 = tilleggsbestemmelser_883_2004;
