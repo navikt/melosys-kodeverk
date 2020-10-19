a76851e8d769bd7365c243aefd32a123b34276b5
Paul M. Ø. Lunde <paul.magne.lunde@nav.no>
2020-10-19 13:47:26 +0200

    Lagt til trygdedekninger for folketrygdeloven

--- src/trygdedekninger.js
+++ src/trygdedekninger.js
@@ -10,6 +10,30 @@ const trygdedekninger = [
   {
     kode: 'FULL_DEKNING_EOSFO',
     term: null
+  },
+  {
+    kode: 'HELSEDEL',
+    term: 'Helsedel'
+  },
+  {
+    kode: 'HELSEDEL_MED_SYKE_OG_FORELDREPENGER',
+    term: 'Helsedel med rett til syke-/foreldrepenger'
+  },
+  {
+    kode: 'PENSJONSDEL',
+    term: 'Pensjonsdel'
+  },
+  {
+    kode: 'HELSE_OG_PENSJONSDEL',
+    term: 'Helse- og pensjonsdel'
+  },
+  {
+    kode: 'HELSE_OG_PENSJONSDEL_MED_SYKE_OG_FORELDREPENGER',
+    term: 'Helse- og pensjonsdel med rett til syke-/foreldrepenger'
+  },
+  {
+    kode: 'FULL_DEKNING_FTRL',
+    term: 'Full dekning'
   }
 ];
 module.exports.trygdedekninger = trygdedekninger;
