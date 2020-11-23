7448e92a92119607557a26b05d899bf305fe9040
Andreas Strand <andreas.strand@nav.no>
2020-11-23 13:25:53 +0100

    Legger til ftrl-begrunnelser

---
+++ src/begrunnelser/folketrygdloven/ftrl_2_8_annet_ledd_begrunnelser.js
@@ -0,0 +1,28 @@
+const ftrl_2_8_annet_ledd_begrunnelser = [
+  {
+    kode: 'ANSATT_I_MULTINASJONALT_SELSKAP',
+    term: 'Arbeider i multinasjonalt selskap',
+  },
+  {
+    kode: 'ANSATT_I_INTERNASJONAL_ORGANISASJON',
+    term: 'Arbeider i internasjonal organisasjon',
+  },
+  {
+    kode: 'MEDFOLGENDE_SOM_ARBEIDER_FRA_HJEMMEKONTOR',
+    term: 'Medfølgende som arbeider på hjemmekontor',
+  },
+  {
+    kode: 'SJOFOLK_SOM_ER_BOSATT_I_UTLANDET',
+    term: 'Arbeider på utenlandskregistrert skip',
+  },
+  {
+    kode: 'ANSATT_I_UTENLANDSK_SELSKAP',
+    term: 'Arbeider i utenlandsk selskap',
+  },
+  {
+    kode: 'ANSATT_I_NORSK_VIRKSOMHET_IKKE_UTSENDT',
+    term: 'Arbeider i norsk virksomhet, men ikke utsendt',
+  }
+];
+
+module.exports.ftrl_2_8_annet_ledd_begrunnelser = ftrl_2_8_annet_ledd_begrunnelser;
--- src/begrunnelser/folketrygdloven/index.js
+++ src/begrunnelser/folketrygdloven/index.js
@@ -1,9 +1,13 @@
 const { avslag } = require('./avslag');
 const { endret_unntaksperiode } = require('./endretunntaksperiode');
+const { ftrl_2_8_foerste_bokstav_a_begrunnelser } = require('./ftrl_2_8_foerste_bokstav_a_begrunnelser');
+const { ftrl_2_8_annet_ledd_begrunnelser } = require('./ftrl_2_8_annet_ledd_begrunnelser');
 
 const folketrygdloven = {
   avslag,
   endret_unntaksperiode,
+  ftrl_2_8_foerste_bokstav_a_begrunnelser,
+  ftrl_2_8_annet_ledd_begrunnelser
 };
 
 module.exports.folketrygdloven = folketrygdloven;
