1fbdc4a6cbe124b2169e22a01f4226e89a4f3f7a
janitamarielle <janita.marielle.johansen@nav.no>
2021-01-13 10:04:20 +0100

    Nye begrunnelse-kodeverk for medfølgende familie i FTLR

--- src/begrunnelser/folketrygdloven/index.js
+++ src/begrunnelser/folketrygdloven/index.js
@@ -2,12 +2,16 @@ const { avslag } = require('./avslag');
 const { endret_unntaksperiode } = require('./endretunntaksperiode');
 const { ftrl_2_8_forutgaaende_trygdetid_begrunnelser } = require('./ftrl_2_8_forutgaaende_trygdetid_begrunnelser');
 const { ftrl_2_8_naer_tilknytning_norge_begrunnelser } = require('./ftrl_2_8_naer_tilknytning_norge_begrunnelser');
+const { medfolgende_barn_begrunnelser_ftrl } = require('./medfolgende_barn_begrunnelser_ftrl');
+const { medfolgende_ektefelle_samboer_begrunnelser_ftrl } = require('./medfolgende_ektefelle_samboer_begrunnelser_ftrl');
 
 const folketrygdloven = {
   avslag,
   endret_unntaksperiode,
   ftrl_2_8_forutgaaende_trygdetid_begrunnelser,
-  ftrl_2_8_naer_tilknytning_norge_begrunnelser
+  ftrl_2_8_naer_tilknytning_norge_begrunnelser,
+  medfolgende_barn_begrunnelser_ftrl,
+  medfolgende_ektefelle_samboer_begrunnelser_ftrl
 };
 
 module.exports.folketrygdloven = folketrygdloven;
