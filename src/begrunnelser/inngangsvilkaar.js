3305a9cae20a8eccdf02f0c64775f71967322e98
JohanSakshaug <46343153+JohanSakshaug@users.noreply.github.com>
2021-05-05 11:04:22 +0200

    Kode for overstyring inngangsvilkår
    Legg til kode for å si om saksbehandler har overstyrt vurdering av inngangsvilkår

--- src/begrunnelser/inngangsvilkaar.js
+++ src/begrunnelser/inngangsvilkaar.js
@@ -7,6 +7,10 @@ const inngangsvilkaar = [
   {
     kode: 'TEKNISK_FEIL',
     term: 'Det oppsto en teknisk feil ved automatisk vurdering av inngangsvilkår.',
+  },
+  {
+    kode: 'OVERSTYRT_AV_SAKSBEHANDLER',
+    term: null,
   }
 ];
 module.exports.inngangsvilkaar = inngangsvilkaar;
