c9347a185e84d910e3d9f3ab89a490d7421def4f
Paul M. Ø. Lunde <paul.magne.lunde@nav.no>
2021-08-11 14:13:32 +0200

    Lagt til nytt kodeverk Avtaleland

---
+++ src/avtaleland.js
@@ -0,0 +1,63 @@
+/**
+ * Kodeverk/avtaleland
+ * @module
+ */
+const avtaleland = [
+  {
+    kode: 'AU',
+    term: 'Australia',
+  },
+  {
+    kode: 'BA',
+    term: 'Bosnia-Hercegovina',
+  },
+  {
+    kode: 'CA',
+    term: 'Canada',
+  },
+  {
+    kode: 'CL',
+    term: 'Chile',
+  },
+  {
+    kode: 'IN',
+    term: 'India',
+  },
+  {
+    kode: 'IL',
+    term: 'Israel',
+  },
+  {
+    kode: 'ME',
+    term: 'Montenegro',
+  },
+  {
+    kode: 'RS',
+    term: 'Serbia',
+  },
+  {
+    kode: 'GB',
+    term: 'Storbritannia',
+  },
+  {
+    kode: 'JE',
+    term: 'Jersey'
+  },
+  {
+    kode: 'IM',
+    term: 'Isle of Man'
+  },
+  {
+    kode: 'TR',
+    term: 'Tyrkia',
+  },
+  {
+    kode: 'US',
+    term: 'USA',
+  },
+  {
+    kode: 'CH',
+    term: 'Sveits',
+  },
+];
+module.exports.avtaleland = avtaleland;
