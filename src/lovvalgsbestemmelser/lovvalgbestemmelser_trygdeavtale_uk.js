9d36dbcf89188f4168718ac815a6ea65b02bf679
janitamarielle <janita.marielle.johansen@nav.no>
2021-08-17 12:39:07 +0200

    Nytt lovvalgbestemmelser_trygdeavtale_uk kodeverk

---
+++ src/lovvalgsbestemmelser/lovvalgbestemmelser_trygdeavtale_uk.js
@@ -0,0 +1,23 @@
+const lovvalgbestemmelser_trygdeavtale_uk = [
+  {
+    kode: 'UK_ART6_1',
+    term: 'Utsendt arbeidstaker - artikkel 6 nr. 1',
+  },
+  {
+    kode: 'UK_ART6_5',
+    term: 'Utsendt til kontinentalsokkel - artikkel 6 nr. 5',
+  },
+  {
+    kode: 'UK_ART7',
+    term: 'Arbeid på skip - artikkel 7',
+  },
+  {
+    kode: 'UK_ART8_2',
+    term: 'Offentlig tjenesteperson - artikkel 8 nr. 2',
+  },
+  {
+    kode: 'UK_ART9',
+    term: 'Innvilgelse unntak - artikkel 9',
+  }
+];
+module.exports.lovvalgbestemmelser_trygdeavtale_uk = lovvalgbestemmelser_trygdeavtale_uk;
