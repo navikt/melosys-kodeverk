f963fc63f0861d9e64b8ab59cd5f57b98f523d01
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-09-30 08:59:41 +0200

    La til ny type for sivilstand fra PDL

---
+++ src/sivilstandtyper.js
@@ -0,0 +1,48 @@
+/**
+ * Kodeverk/sivilstandtyper
+ * @module
+ */
+const sivilstandtyper = [
+  {
+    kode: 'ENKE_ELLER_ENKEMANN',
+    term: 'Enke eller enkemann'
+  },
+  {
+    kode: 'GIFT',
+    term: 'Gift'
+  },
+  {
+    kode: 'GJENLEVENDE_PARTNER',
+    term: 'Gjenlevende partner'
+  },
+  {
+    kode: 'REGISTRERT_PARTNER',
+    term: 'Registrert partner'
+  },
+  {
+    kode: 'SEPARERT',
+    term: 'Separert'
+  },
+  {
+    kode: 'SEPARERT_PARTNER',
+    term: 'Separert partner'
+  },
+  {
+    kode: 'SKILT',
+    term: 'Skilt'
+  },
+  {
+    kode: 'SKILT_PARTNER',
+    term: 'Skilt partner'
+  },
+  {
+    kode: 'UGIFT',
+    term: 'Ugift'
+  },
+  {
+    kode: 'UOPPGITT',
+    term: 'Uoppgitt'
+  },
+];
+
+module.exports.personstatuser = sivilstandtyper;
