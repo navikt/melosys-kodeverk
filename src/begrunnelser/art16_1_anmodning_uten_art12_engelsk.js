c760f3ee2ee49d143cb820f15a74a2e0953cf846
Francois Allix <32903762+frans-a@users.noreply.github.com>
2021-12-01 14:37:29 +0100

    Art. 16 hjemmekontor næringsdrivende
    Ny begrunnelse for å anmode om art 16 for selvstendig næringsdrivende i forkortet flyt : ART16_1_ANMODNING_UTEN_ART12_BEGRUNNELSER

--- src/begrunnelser/art16_1_anmodning_uten_art12_engelsk.js
+++ src/begrunnelser/art16_1_anmodning_uten_art12_engelsk.js
@@ -14,21 +14,28 @@ const art16_1_anmodning_uten_art12_engelsk = [
   {
     kode: 'HJEMMEKONTOR_BEGRENSET_PERIODE',
     term: 'Will briefly be working in a home office in your country for her/his Norwegian employer, and is not considered to be covered under Art. 12 No 1 of Reg 883/04.',
-  },  {
+  },
+  {
+    kode: 'HJEMMEKONTOR_NAERINGSDRIVENDE',
+    term: 'Will briefly be working in a home office in your country as a self-employed person, and is not considered to be covered under Art. 12 No 2 of Reg 883/04.',
+  },
+  {
     kode: 'DELTIDSARBEID_I_UTLANDET_MOTTAR_AAP',
     term: 'Receives work assessment allowance from Norway while working part time abroad as part of the rehabilitation plan. This allowance is a cash benefit under article 11(2) of Reg. (EC) No 883/2004, given while the person\'s work capacity is being assessed.',
   },
   {
     kode: 'DEN_NORSKE_SKOLEN',
     term: 'Working in your country for the Norwegian school, which is financially supported by the Norwegian Government.',
-  },  {
+  },
+  {
     kode: 'SJOEMANNSKIRKEN',
     term: 'Working for Sjømannskirken (The Norwegian Seamen’s Church), which is a nonprofit organization receiving financial support from the Norwegian Government.',
   },
   {
     kode: 'STUDENT_I_NORGE_RELEVANT_ARBEID',
     term: 'The person is usually a student in Norway and will do study-related work in your country.',
-  },  {
+  },
+  {
     kode: 'STUDENT_I_UTLANDET_RELEVANT_ARBEID',
     term: 'The person is usually covered by the Norwegian social security legislation under the article 11(3)(e) as a student in your country, and will be doing study-related work there.',
   },
