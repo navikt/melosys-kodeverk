ab98c6769bfc352e0d3f4b6c43054da1ea81d101
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-06-14 17:04:58 +0200

    Introduser sakstema

---
+++ src/sakstemaer.js
@@ -0,0 +1,19 @@
+/**
+ * Kodeverk/sakstema
+ * @module
+ */
+const sakstemaer = [
+  {
+    kode: 'MEDLEMSKAP_LOVVALG',
+    term: 'Medlemskap og lovvalg',
+  },
+  {
+    kode: 'UNNTAK',
+    term: 'Unntak',
+  },
+  {
+    kode: 'TRYGDEAVGIFT',
+    term: 'Trygdeavgift',
+  }
+];
+module.exports.sakstemaer = sakstemaer;
