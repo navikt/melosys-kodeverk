dfb238bd2cb85570cf0c5b207b1e92e61e2e7707
Isa Salem Hame <wanghattan@gmail.com>
2022-06-23 02:18:43 +0200

    Introduserer distribusjonstyper

---
+++ src/distribusjonstyper.js
@@ -0,0 +1,15 @@
+const distribusjonstyper = [
+  {
+    kode: 'VEDTAK',
+    term: 'VEDTAK'
+  },
+  {
+    kode: 'VIKTIG',
+    term: 'VIKTIG'
+  },
+  {
+    kode: 'ANNET',
+    term: 'ANNET'
+  }
+];
+module.exports.distribusjonstyper = distribusjonstyper;
--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ const { vilkaar } = require('./vilkaar');
 const { vurderingsutfall_trygdeavgift_norsk_inntekt } = require('./vurderingsutfall_trygdeavgift_norsk_inntekt');
 const { vurderingsutfall_trygdeavgift_utenlandsk_inntekt } = require('./vurderingsutfall_trygdeavgift_utenlandsk_inntekt');
 const { yrker } = require('./yrker');
+const { distribusjonstyper } = require('./distribusjonstyper');
 
 const Transform  = require('./transform');
 
@@ -84,6 +85,7 @@ const KodeTermObjects = {
   vurderingsutfall_trygdeavgift_norsk_inntekt,
   vurderingsutfall_trygdeavgift_utenlandsk_inntekt,
   yrker,
+  distribusjonstyper
 };
 
 const Koder = Transform.kodeverk2KodeSet(KodeTermObjects);
