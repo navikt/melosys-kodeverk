bc25f8398dde7694c88a164d88b14fcd1f7063d5
Eirik Bergande <eirik.bergande@nav.no>
2022-10-18 11:39:59 +0200

    4664 Legger til lovvalgsbestemmeler for usa trygdeavtale (#250)
    Legger til lovvalgsbestemmeler for usa trygdeavtale

--- src/lovvalgsbestemmelser/index.js
+++ src/lovvalgsbestemmelser/index.js
@@ -1,8 +1,9 @@
-const { lovvalgbestemmelser_883_2004 } = require('./lovvalgbestemmelser_883_2004');
-const { lovvalgbestemmelser_987_2009 } = require('./lovvalgbestemmelser_987_2009');
-const { tilleggsbestemmelser_883_2004 } = require('./tilleggsbestemmelser_883_2004');
-const { overgangsregelbestemmelser } = require('./overgangsregelbestemmelser');
-const { lovvalgbestemmelser_trygdeavtale_uk } = require('./lovvalgbestemmelser_trygdeavtale_uk');
+const {lovvalgbestemmelser_883_2004} = require('./lovvalgbestemmelser_883_2004');
+const {lovvalgbestemmelser_987_2009} = require('./lovvalgbestemmelser_987_2009');
+const {tilleggsbestemmelser_883_2004} = require('./tilleggsbestemmelser_883_2004');
+const {overgangsregelbestemmelser} = require('./overgangsregelbestemmelser');
+const {lovvalgbestemmelser_trygdeavtale_uk} = require('./lovvalgbestemmelser_trygdeavtale_uk');
+const {lovvalgbestemmelser_trygdeavtale_usa} = require('./lovvalgbestemmelser_trygdeavtale_usa');
 
 const lovvalgsbestemmelser = {
   lovvalgbestemmelser_883_2004,
@@ -10,5 +11,6 @@ const lovvalgsbestemmelser = {
   tilleggsbestemmelser_883_2004,
   overgangsregelbestemmelser,
   lovvalgbestemmelser_trygdeavtale_uk,
+  lovvalgbestemmelser_trygdeavtale_usa,
 };
 module.exports.lovvalgsbestemmelser = lovvalgsbestemmelser;
