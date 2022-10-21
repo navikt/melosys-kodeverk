95685a6c0609f02723886700af6d18eb64b228be
Eirik Bergande <eirik.bergande@nav.no>
2022-10-21 12:05:57 +0200

    Oppretter land_iso2

---
+++ src/land_iso2.js
@@ -0,0 +1,203 @@
+/**
+ * Kodeverk/landkoder
+ * @module
+ */
+const land_iso2 = [
+  {
+    kode: 'BE',
+    term: 'Belgia',
+  },
+  {
+    kode: 'BG',
+    term: 'Bulgaria',
+  },
+  {
+    kode: 'DK',
+    term: 'Danmark',
+  },
+  {
+    kode: 'EE',
+    term: 'Estland',
+  },
+  {
+    kode: 'FI',
+    term: 'Finland',
+  },
+  {
+    kode: 'FR',
+    term: 'Frankrike',
+  },
+  {
+    kode: 'FO',
+    term: 'Færøyene',
+  },
+  {
+    kode: 'GL',
+    term: 'Grønland',
+  },
+  {
+    kode: 'GR',
+    term: 'Hellas',
+  },
+  {
+    kode: 'IE',
+    term: 'Irland',
+  },
+  {
+    kode: 'IS',
+    term: 'Island',
+  },
+  {
+    kode: 'IT',
+    term: 'Italia',
+  },
+  {
+    kode: 'CY',
+    term: 'Kypros',
+  },
+  {
+    kode: 'LV',
+    term: 'Latvia',
+  },
+  {
+    kode: 'LI',
+    term: 'Liechtenstein',
+  },
+  {
+    kode: 'LT',
+    term: 'Litauen',
+  },
+  {
+    kode: 'MT',
+    term: 'Malta',
+  },
+  {
+    kode: 'NO',
+    term: 'Norge',
+  },
+  {
+    kode: 'PL',
+    term: 'Polen',
+  },
+  {
+    kode: 'RO',
+    term: 'Romania',
+  },
+  {
+    kode: 'SK',
+    term: 'Slovakia',
+  },
+  {
+    kode: 'ES',
+    term: 'Spania',
+  },
+  {
+    kode: 'SJ',
+    term: 'Svalbard og Jan Mayen',
+  },
+  {
+    kode: 'SE',
+    term: 'Sverige',
+  },
+  {
+    kode: 'CZ',
+    term: 'Tsjekkia',
+  },
+  {
+    kode: 'DE',
+    term: 'Tyskland',
+  },
+  {
+    kode: 'AX',
+    term: 'Åland',
+  },
+  {
+    kode: 'AU',
+    term: 'Australia',
+  },
+  {
+    kode: 'BA',
+    term: 'Bosnia-Hercegovina',
+  },
+  {
+    kode: 'CA',
+    term: 'Canada',
+  },
+  {
+    kode: 'CL',
+    term: 'Chile',
+  },
+  {
+    kode: 'IN',
+    term: 'India',
+  },
+  {
+    kode: 'IL',
+    term: 'Israel',
+  },
+  {
+    kode: 'IM',
+    term: 'Isle of Man'
+  },
+  {
+    kode: 'JE',
+    term: 'Jersey'
+  },
+  {
+    kode: 'HR',
+    term: 'Kroatia',
+  },
+  {
+    kode: 'LU',
+    term: 'Luxembourg',
+  },
+  {
+    kode: 'ME',
+    term: 'Montenegro',
+  },
+  {
+    kode: 'NL',
+    term: 'Nederland',
+  },
+  {
+    kode: 'PT',
+    term: 'Portugal',
+  },
+  {
+    kode: 'CA_QC',
+    term: 'Quebec',
+  },
+  {
+    kode: 'RS',
+    term: 'Serbia',
+  },
+  {
+    kode: 'SI',
+    term: 'Slovenia',
+  },
+  {
+    kode: 'GB',
+    term: 'Storbritannia',
+  },
+  {
+    kode: 'CH',
+    term: 'Sveits',
+  },
+  {
+    kode: 'TR',
+    term: 'Tyrkia',
+  },
+  {
+    kode: 'HU',
+    term: 'Ungarn',
+  },
+  {
+    kode: 'US',
+    term: 'USA',
+  },
+  {
+    kode: 'AT',
+    term: 'Østerrike',
+  }
+];
+module.exports.land_iso2 = land_iso2;
