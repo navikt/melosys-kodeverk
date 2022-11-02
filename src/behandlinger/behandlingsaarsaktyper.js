06a7e21f9b291c84c957c886da7e1a1d84e5d8b9
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-11-02 13:34:15 +0100

    Behandlingsaarsaktyper

---
+++ src/behandlinger/behandlingsaarsaktyper.js
@@ -0,0 +1,20 @@
+const behandlingsaarsaktyper = [
+  {
+    kode: 'SØKNAD',
+    term: 'Søknad',
+  },
+  {
+    kode: 'SED',
+    term: 'SED'
+  },
+  {
+    kode: 'HENVENDELSE',
+    term: 'Henvendelse'
+  },
+  {
+    kode: 'ANNET',
+    term: 'Annet'
+  }
+];
+
+module.exports.behandlingsaarsaktyper = behandlingsaarsaktyper;
