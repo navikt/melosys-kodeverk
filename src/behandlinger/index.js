0b5ee8b5dff1e800eb7516aebcb9eeefdf68d9e0
Francois Allix <32903762+frans-a@users.noreply.github.com>
2022-11-02 13:34:15 +0100

    Behandlingsaarsaktyper

--- src/behandlinger/index.js
+++ src/behandlinger/index.js
@@ -1,9 +1,11 @@
+const { behandlingsaarsaktyper } = require('./behandlingsaarsaktyper');
 const { behandlingsresultattyper } = require('./behandlingsresultattyper');
 const { behandlingsstatus } = require('./behandlingsstatus');
 const { behandlingstyper } = require('./behandlingstyper');
 const { behandlingstema } = require('./behandlingstema');
 
 const behandlinger = {
+  behandlingsaarsaktyper,
   behandlingsresultattyper,
   behandlingsstatus,
   behandlingstema,
