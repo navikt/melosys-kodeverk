60a5d772cbe8a83336f69f57f7d6989703de112a
Eirik Bergande <eirik.bergande@nav.no>
2022-11-17 10:30:05 +0100

    5459 Legger til art8 som tilleggsbestemmelse (#267)
    * Legger til art8 som tilleggsbestemmelse
    
    * Tilleggsbestemmelser_canada

--- src/lovvalgsbestemmelser/lovvalgbestemmelser_trygdeavtale_ca.js
+++ src/lovvalgsbestemmelser/lovvalgbestemmelser_trygdeavtale_ca.js
@@ -1,31 +1,23 @@
 const lovvalgbestemmelser_trygdeavtale_ca = [
-    {
-        kode: 'CAN_ART6_2',
-        term: 'Selvstendig næringsdrivende - artikkel 6 nr. 2',
-    },
-    {
-        kode: 'CAN_ART6_2_JF8',
-        term: 'Selvstendig næringsdrivende - arbeid på sokkel (artikkel 6 nr. 2 jf. artikkel 8)',
-    },
-    {
-        kode: 'CAN_ART7',
-        term: 'Utsendt arbeidstaker - artikkel 7',
-    },
-    {
-        kode: 'CAN_ART7_JF8',
-        term: 'Utsendt arbeidstaker - arbeid på sokkel (artikkel 7 jf. artikkel 8)',
-    },
-    {
-        kode: 'CAN_ART9',
-        term: 'Arbeid på skip - artikkel 9',
-    },
-    {
-        kode: 'CAN_ART10',
-        term: 'Offentlig tjenesteperson - artikkel 10',
-    },
-    {
-        kode: 'CAN_ART11',
-        term: 'Innvilgelse unntak - artikkel 11',
-    },
+  {
+    kode: 'CAN_ART6_2',
+    term: 'Selvstendig næringsdrivende - artikkel 6 nr. 2',
+  },
+  {
+    kode: 'CAN_ART7',
+    term: 'Utsendt arbeidstaker - artikkel 7',
+  },
+  {
+    kode: 'CAN_ART9',
+    term: 'Arbeid på skip - artikkel 9',
+  },
+  {
+    kode: 'CAN_ART10',
+    term: 'Offentlig tjenesteperson - artikkel 10',
+  },
+  {
+    kode: 'CAN_ART11',
+    term: 'Innvilgelse unntak - artikkel 11',
+  },
 ];
 module.exports.lovvalgbestemmelser_trygdeavtale_ca = lovvalgbestemmelser_trygdeavtale_ca;
