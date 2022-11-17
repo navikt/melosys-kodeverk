522fcc8689985da56ba58ed33b8e9f12765f2dec
Eirik Bergande <eirik.bergande@nav.no>
2022-11-17 10:15:59 +0100

    Legger til art8 som tilleggsbestemmelse

--- src/lovvalgsbestemmelser/index.js
+++ src/lovvalgsbestemmelser/index.js
@@ -5,14 +5,16 @@ const {overgangsregelbestemmelser} = require('./overgangsregelbestemmelser');
 const {lovvalgbestemmelser_trygdeavtale_uk} = require('./lovvalgbestemmelser_trygdeavtale_uk');
 const {lovvalgbestemmelser_trygdeavtale_usa} = require('./lovvalgbestemmelser_trygdeavtale_usa');
 const {lovvalgbestemmelser_trygdeavtale_ca} = require('./lovvalgbestemmelser_trygdeavtale_ca');
+const {tilleggsbestemmelser_trygdeavtale_ca} = require('./tilleggsbestemmelser_trygdeavtale_ca');
 
 const lovvalgsbestemmelser = {
-    lovvalgbestemmelser_883_2004,
-    lovvalgbestemmelser_987_2009,
-    tilleggsbestemmelser_883_2004,
-    overgangsregelbestemmelser,
-    lovvalgbestemmelser_trygdeavtale_uk,
-    lovvalgbestemmelser_trygdeavtale_usa,
-    lovvalgbestemmelser_trygdeavtale_ca,
+  lovvalgbestemmelser_883_2004,
+  lovvalgbestemmelser_987_2009,
+  tilleggsbestemmelser_883_2004,
+  overgangsregelbestemmelser,
+  lovvalgbestemmelser_trygdeavtale_uk,
+  lovvalgbestemmelser_trygdeavtale_usa,
+  lovvalgbestemmelser_trygdeavtale_ca,
+  tilleggsbestemmelser_trygdeavtale_ca,
 };
 module.exports.lovvalgsbestemmelser = lovvalgsbestemmelser;
