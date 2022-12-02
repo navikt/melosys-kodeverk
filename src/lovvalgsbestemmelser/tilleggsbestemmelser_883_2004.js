240f19c5767ba179d8b69149d84fa39d1970ee95
barstadEkstern <oystein.madsen.barstad@nav.no>
2022-12-02 14:15:50 +0100

    Oppdatert kodeverk tilleggsbestemmelser_883_2004

--- src/lovvalgsbestemmelser/tilleggsbestemmelser_883_2004.js
+++ src/lovvalgsbestemmelser/tilleggsbestemmelser_883_2004.js
@@ -2,23 +2,23 @@
 const tilleggsbestemmelser_883_2004 = [
   {
     kode: 'FO_883_2004_ART11_2',
-    term: 'artikkel 11 nr. 2',
+    term: 'Rfo. 883/2004 art.11(2)',
   },
   {
     kode: 'FO_883_2004_ART11_4_1',
-    term: 'artikkel 11 nr. 4 første punktum',
+    term: 'Rfo. 883/2004 art.11(4)(1)',
   },
   {
     kode: 'FO_883_2004_ART11_5',
-    term: 'artikkel 11 nr. 5',
+    term: 'Rfo. 883/2004 art.11(5)',
   },
   {
     kode: 'FO_883_2004_ART87_8',
-    term: 'artikkel 87 nr. 8',
+    term: 'Rfo. 883/2004 art.87(8)',
   },
   {
     kode: 'FO_883_2004_ART87A',
-    term: 'artikkel 87a',
+    term: 'Rfo. 883/2004 art.87(a)',
   }
 ];
 module.exports.tilleggsbestemmelser_883_2004 = tilleggsbestemmelser_883_2004;
