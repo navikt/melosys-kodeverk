3e77b1d829da13e34786ed9c0033807d12180607
barstadEkstern <oystein.madsen.barstad@nav.no>
2022-12-02 14:12:51 +0100

    Oppdatert kodeverk LovvalgsBestemmelser_883_2004

--- src/lovvalgsbestemmelser/lovvalgbestemmelser_883_2004.js
+++ src/lovvalgsbestemmelser/lovvalgbestemmelser_883_2004.js
@@ -6,91 +6,91 @@ const lovvalgbestemmelser_883_2004 = [
   },
   {
     kode: 'FO_883_2004_ART11_3A',
-    term: 'artikkel 11 nr. 3 bokstav a',
+    term: 'Rfo. 883/2004 art.11(3)(a)',
   },
   {
     kode: 'FO_883_2004_ART11_3B',
-    term: 'artikkel 11 nr. 3 bokstav b',
+    term: 'Rfo. 883/2004 art.11(3)(b)',
   },
   {
     kode: 'FO_883_2004_ART11_3C',
-    term: 'artikkel 11 nr. 3 bokstav c',
+    term: 'Rfo. 883/2004 art.11(3)(c)',
   },
   {
     kode: 'FO_883_2004_ART11_3D',
-    term: 'artikkel 11 nr. 3 bokstav d',
+    term: 'Rfo. 883/2004 art.11(3)(d)',
   },
   {
     kode: 'FO_883_2004_ART11_3E',
-    term: 'artikkel 11 nr. 3 bokstav e',
+    term: 'Rfo. 883/2004 art.11(3)(e)',
   },
   {
     kode: 'FO_883_2004_ART11_4',
-    term: 'artikkel 11 nr. 4',
+    term: 'Rfo. 883/2004 art.11(4)',
   },
   {
     kode: 'FO_883_2004_ART11_4_2',
-    term: 'artikkel 11 nr. 4 andre punktum',
+    term: 'Rfo. 883/2004 art.11(4)(2)',
   },
   {
     kode: 'FO_883_2004_ART12_1',
-    term: 'artikkel 12 nr. 1',
+    term: 'Rfo. 883/2004 art.12(1)',
   },
   {
     kode: 'FO_883_2004_ART12_2',
-    term: 'artikkel 12 nr. 2',
+    term: 'Rfo. 883/2004 art.12(2)',
   },
   {
     kode: 'FO_883_2004_ART13_1A',
-    term: 'artikkel 13 nr. 1 bokstav a',
+    term: 'Rfo. 883/2004 art.13(1)(a)',
   },
   {
     kode: 'FO_883_2004_ART13_1B1',
-    term: 'artikkel 13 nr. 1 bokstav b i)',
+    term: 'Rfo. 883/2004 art.13(1)(b)(i)',
   },
   {
     kode: 'FO_883_2004_ART13_1B2',
-    term: 'artikkel 13 nr. 1 bokstav b ii)',
+    term: 'Rfo. 883/2004 art.13(1)(b)(ii)',
   },
   {
     kode: 'FO_883_2004_ART13_1B3',
-    term: 'artikkel 13 nr. 1 bokstav b iii)',
+    term: 'Rfo. 883/2004 art. 13(1)(b)(iii)',
   },
   {
     kode: 'FO_883_2004_ART13_1B4',
-    term: 'artikkel 13 nr. 1 bokstav b iv)',
+    term: 'Rfo. 883/2004 art.13(1)(b)(iv)',
   },
   {
     kode: 'FO_883_2004_ART13_2A',
-    term: 'artikkel 13 nr. 2 bokstav a',
+    term: 'Rfo. 883/2004 art.13(2)(a)',
   },
   {
     kode: 'FO_883_2004_ART13_2B',
-    term: 'artikkel 13 nr. 2 bokstav b',
+    term: 'Rfo. 883/2004 art.13(2)(b)',
   },
   {
     kode: 'FO_883_2004_ART13_3',
-    term: 'artikkel 13 nr. 3',
+    term: 'Rfo. 883/2004 art.13(3)',
   },
   {
     kode: 'FO_883_2004_ART13_4',
-    term: 'artikkel 13 nr. 4',
+    term: 'Rfo. 883/2004 art.13(4)',
   },
   {
     kode: 'FO_883_2004_ART15',
-    term: 'artikkel 15',
+    term: 'Rfo. 883/2004 art.15',
   },
   {
     kode: 'FO_883_2004_ART16_1',
-    term: 'artikkel 16 nr. 1',
+    term: 'Rfo. 883/2004 art.16(1)',
   },
   {
     kode: 'FO_883_2004_ART16_2',
-    term: 'artikkel 16 nr. 2',
+    term: 'Rfo. 883/2004 art.16(2)',
   },
   {
     kode: 'FO_883_2004_ANNET',
-    term: null,
+    term: 'Rfo. 883/2004 annet',
   }
 ];
 module.exports.lovvalgbestemmelser_883_2004 = lovvalgbestemmelser_883_2004;
