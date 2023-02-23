39e28f3840045135c5d424608b164849e0518b3f
Janita Johansen <janita.marielle.johansen@nav.no>
2023-02-23 12:08:12 +0100

    Reverter UK -> GBR

--- src/lovvalgsbestemmelser/trygdeavtale/lovvalgbestemmelser_trygdeavtale_gb.js
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgbestemmelser_trygdeavtale_gb.js
@@ -1,46 +1,46 @@
 const lovvalgbestemmelser_trygdeavtale_gb = [
   {
-    kode: 'GBR',
+    kode: 'UK',
     term: 'Generell bestemmelse',
   },
   {
-    kode: 'GBR_ART5_4',
+    kode: 'UK_ART5_4',
     term: 'Ikke-yrkesaktiv - artikkel 5 nr. 4',
   },
   {
-    kode: 'GBR_ART6_1',
+    kode: 'UK_ART6_1',
     term: 'Utsendt arbeidstaker - artikkel 6 nr. 1',
   },
   {
-    kode: 'GBR_ART6_2',
+    kode: 'UK_ART6_2',
     term: 'Medfølgende familie til utsendt arbeidstaker - artikkel 6 nr. 2',
   },
   {
-    kode: 'GBR_ART6_5',
+    kode: 'UK_ART6_5',
     term: 'Utsendt til kontinentalsokkel - artikkel 6 nr. 5',
   },
   {
-    kode: 'GBR_ART6_10',
+    kode: 'UK_ART6_10',
     term: 'Reisende personell innen transport - artikkel 6 nr. 10',
   },
   {
-    kode: 'GBR_ART7_1',
+    kode: 'UK_ART7_1',
     term: 'Arbeid på skip - artikkel 7 nr. 1',
   },
   {
-    kode: 'GBR_ART7_3',
+    kode: 'UK_ART7_3',
     term: 'Utsendt til skip - artikkel 7 nr. 3',
   },
   {
-    kode: 'GBR_ART8_2',
+    kode: 'UK_ART8_2',
     term: 'Offentlig tjenesteperson - artikkel 8 nr. 2',
   },
   {
-    kode: 'GBR_ART8_5',
+    kode: 'UK_ART8_5',
     term: 'Medfølgende familie til offentlig tjenesteperson - artikkel 8 nr. 5',
   },
   {
-    kode: 'GBR_ART9',
+    kode: 'UK_ART9',
     term: 'Unntak - artikkel 9',
   }
 ];
