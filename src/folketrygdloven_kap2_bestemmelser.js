fc7acbc224e0e4ec54a95b335a3f3cdf00a45182
Thang Phan <thang.quoc.ly.phan@nav.no>
2023-02-27 11:32:47 +0100

    5632: Legg inn alle bestemmelser (#288)

--- src/folketrygdloven_kap2_bestemmelser.js
+++ src/folketrygdloven_kap2_bestemmelser.js
@@ -4,8 +4,24 @@
  */
 const folketrygdloven_kap2_bestemmelser = [
   {
-    kode: 'FTRL_KAP2_2_1',
-    term: null
+    kode: 'FTRL_KAP2_2_1_FØRSTE_LEDD',
+    term: '§ 2-1 første ledd (bosatt i Norge)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_1_FJERDE_LEDD',
+    term: '§ 2-1 fjerde ledd (midlertidig fravær under 12 mnd)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_2',
+    term: '§ 2-2 (arbeidstaker i Norge)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_3_FØRSTE_LEDD',
+    term: '§ 2-3 første ledd (pliktig før opphold)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_3_ANDRE_LEDD',
+    term: '§ 2-3 andre ledd (arbeid for norsk arbeidsgiver)'
   },
   {
     kode: 'FTRL_KAP2_2_8',
@@ -44,8 +60,68 @@ const folketrygdloven_kap2_bestemmelser = [
     term: '§ 2-8 femte ledd (virkningstidspunkt)'
   },
   {
-    kode: 'FTRL_KAP2_2_9',
-    term: null
+    kode: 'FTRL_KAP2_2_5',
+    term: '§ 2-5 (pliktig medlemskap utenfor Norge)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_A',
+    term: '§ 2-5 første ledd bokstav a (offentlig ansatt)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_B',
+    term: '§ 2-5 første ledd bokstav b (arbeider for offentlig ansatt)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_C',
+    term: '§ 2-5 første ledd bokstav c (i forsvarets tjeneste)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_D',
+    term: '§ 2-5 første ledd bokstav d (fredskorpsdeltakere/ tjenestegjøring i utviklingsland)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_E',
+    term: '§ 2-5 første ledd bokstav e (NATOs sivile krigstidsorganer)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_F',
+    term: '§ 2-5 første ledd bokstav f (norskregistrert skip)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_G',
+    term: '§ 2-5 første ledd bokstav g (norsk sivilt luftfartsselskap)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_FØRSTE_LEDD_H',
+    term: '§ 2-5 første ledd bokstav h (student)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_5_ANDRE_LEDD',
+    term: '§ 2-5 andre ledd (familiemedlemmer)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_6_FØRSTE_LEDD_A',
+    term: '§ 2-6 (norsk skip i utenriksfart)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_6_FØRSTE_LEDD_B',
+    term: '§ 2-6 (norsk fiskefartøy)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_6_FØRSTE_LEDD_C',
+    term: '§ 2-6 (norsk sivilt luftfartsselskap)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_7_FØRSTE_LEDD',
+    term: '§ 2-7 første ledd (opphold i Norge)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_7_FJERDE_LEDD',
+    term: '§ 2-7 fjerde ledd (familiemedlemmer)'
+  },
+  {
+    kode: 'FTRL_KAP2_2_7A',
+    term: '§ 2-7a (frivillig medlemskap på utenlandsk skip)'
   }
 ];
 module.exports.folketrygdloven_kap2_bestemmelser = folketrygdloven_kap2_bestemmelser;
