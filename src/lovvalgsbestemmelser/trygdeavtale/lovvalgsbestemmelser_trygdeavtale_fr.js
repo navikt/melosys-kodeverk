05262e09f279dbadad97a96c97447e9d6faa4c09
Isa Salem Hame <isa.salem.hame@nav.no>
2023-02-28 16:24:05 +0100

    Legger til Italia og Frankrikes generelle bestemmelse (#291)

---
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_fr.js
@@ -0,0 +1,7 @@
+const lovvalgsbestemmelser_trygdeavtale_fr = [
+  {
+    kode: 'FRA',
+    term: 'Generell bestemmelse',
+  },
+];
+module.exports.lovvalgsbestemmelser_trygdeavtale_fr = lovvalgsbestemmelser_trygdeavtale_fr;
---
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_it.js
@@ -0,0 +1,7 @@
+const lovvalgsbestemmelser_trygdeavtale_it = [
+  {
+    kode: 'ITA',
+    term: 'Generell bestemmelse',
+  },
+];
+module.exports.lovvalgsbestemmelser_trygdeavtale_it = lovvalgsbestemmelser_trygdeavtale_it;
