9223d8e0b444ca2f74edcf20f61891695a8f709b
Isa Salem Hame <isa.salem.hame@nav.no>
2023-03-01 14:40:55 +0100

    5687: Flytter CHL_ART8 til lovvalgsbestemmelser (#294)

--- src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_cl.js
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_cl.js
@@ -15,5 +15,9 @@ const lovvalgsbestemmelser_trygdeavtale_cl = [
     kode: 'CHL_ART6_4',
     term: 'Medfølgende familie til utsendt arbeidstaker - artikkel 6 nr. 4',
   },
+  {
+    kode: 'CHL_ART8',
+    term: 'Unntak - artikkel 8',
+  },
 ];
 module.exports.lovvalgsbestemmelser_trygdeavtale_cl = lovvalgsbestemmelser_trygdeavtale_cl;
