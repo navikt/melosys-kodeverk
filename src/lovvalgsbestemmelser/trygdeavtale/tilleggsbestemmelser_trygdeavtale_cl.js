7f904e96edd63addcf571d8982a0260bca197396
Isa Salem Hame <wanghattan@gmail.com>
2023-03-01 14:09:56 +0100

    5687: Flytter CHL_ART8 til lovvalgsbestemmelser

--- src/lovvalgsbestemmelser/trygdeavtale/tilleggsbestemmelser_trygdeavtale_cl.js
+++ src/lovvalgsbestemmelser/trygdeavtale/tilleggsbestemmelser_trygdeavtale_cl.js
@@ -3,9 +3,5 @@ const tilleggsbestemmelser_trygdeavtale_cl = [
     kode: 'CHL_ART7',
     term: 'Arbeid på sokkel/skip - artikkel 7',
   },
-  {
-    kode: 'CHL_ART8',
-    term: 'Unntak - artikkel 8',
-  },
 ];
 module.exports.tilleggsbestemmelser_trygdeavtale_cl = tilleggsbestemmelser_trygdeavtale_cl;
