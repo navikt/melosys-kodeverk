cbde36c0b3b0061b8899d8a8aab01e1a1b730a2f
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2023-03-13 15:14:17 +0100

    5769 Lagt til lovvalgsbestemmelser for trygdeavtale for Slovenia og Koratia (#299)

---
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_si.js
@@ -0,0 +1,31 @@
+const lovvalgsbestemmelser_trygdeavtale_si = [
+  {
+    kode: 'SVN',
+    term: 'Generell bestemmelse',
+  },
+  {
+    kode: 'SVN_ART3',
+    term: 'Hovedregel - Artikkel 3',
+  },
+  {
+    kode: 'SVN_ART4',
+    term: 'Utsendt arbeidstaker - Artikkel 4',
+  },
+  {
+    kode: 'SVN_ART4_A',
+    term: 'Medfølgende familie til utsendt arbeidstaker - artikkel 4 a',
+  },
+  {
+    kode: 'SVN_ART5',
+    term: 'Offentlig tjenesteperson og familiemedlemmer - artikkel 5',
+  },
+  {
+    kode: 'SVN_ART6',
+    term: 'Arbeid på skip - artikkel 6',
+  },
+  {
+    kode: 'SVN_ART7',
+    term: 'Unntak - artikkel 7',
+  },
+];
+module.exports.lovvalgsbestemmelser_trygdeavtale_si = lovvalgsbestemmelser_trygdeavtale_si;
