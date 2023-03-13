af81e9d1ad52c69e4c13831062e87f3dfbfdb2d6
martonSkjevelandNav <marton.skjeveland@nav.no>
2023-03-13 14:35:41 +0100

    Lagt til lovvalgsbestemmelser for trygdeavtale for Slovenia og Koratia

---
+++ src/lovvalgsbestemmelser/trygdeavtale/lovvalgsbestemmelser_trygdeavtale_hr.js
@@ -0,0 +1,31 @@
+const lovvalgsbestemmelser_trygdeavtale_hr = [
+  {
+    kode: 'HRV',
+    term: 'Generell bestemmelse',
+  },
+  {
+    kode: 'HRV_ART3',
+    term: 'Hovedregel - Artikkel 3',
+  },
+  {
+    kode: 'HRV_ART4',
+    term: 'Utsendt arbeidstaker - Artikkel 4',
+  },
+  {
+    kode: 'HRV_ART4_A',
+    term: 'Medfølgende familie til utsendt arbeidstaker - artikkel 4 a',
+  },
+  {
+    kode: 'HRV_ART5',
+    term: 'Offentlig tjenesteperson og familiemedlemmer - artikkel 5',
+  },
+  {
+    kode: 'HRV_ART6',
+    term: 'Arbeid på skip - artikkel 6',
+  },
+  {
+    kode: 'HRV_ART7',
+    term: 'Unntak - artikkel 7',
+  },
+];
+module.exports.lovvalgsbestemmelser_trygdeavtale_hr = lovvalgsbestemmelser_trygdeavtale_hr;
