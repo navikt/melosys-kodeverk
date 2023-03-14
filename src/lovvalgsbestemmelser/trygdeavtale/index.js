61756dfb56a4002d09fbce42f4dbb6b84384f1d5
Marton Skjæveland <56435736+martonSkjevelandNav@users.noreply.github.com>
2023-03-14 11:27:19 +0100

    5769 Legger til de nye trygdeavtaleland verdiene (#300)
    * Legger til de nye trygdeavtaleland verdiene
    
    * La til de nye landene i schema
    
    * La til nye land i application.yml
    
    * Oppdaterer versjon manuelt da det ikke ble gjort automatisk

--- src/lovvalgsbestemmelser/trygdeavtale/index.js
+++ src/lovvalgsbestemmelser/trygdeavtale/index.js
@@ -7,12 +7,14 @@ const {lovvalgsbestemmelser_trygdeavtale_cl} = require('./lovvalgsbestemmelser_t
 const {lovvalgsbestemmelser_trygdeavtale_fr} = require('./lovvalgsbestemmelser_trygdeavtale_fr');
 const {lovvalgsbestemmelser_trygdeavtale_gb} = require('./lovvalgsbestemmelser_trygdeavtale_gb');
 const {lovvalgsbestemmelser_trygdeavtale_gr} = require('./lovvalgsbestemmelser_trygdeavtale_gr');
+const {lovvalgsbestemmelser_trygdeavtale_hr} = require('./lovvalgsbestemmelser_trygdeavtale_hr');
 const {lovvalgsbestemmelser_trygdeavtale_il} = require('./lovvalgsbestemmelser_trygdeavtale_il');
 const {lovvalgsbestemmelser_trygdeavtale_in} = require('./lovvalgsbestemmelser_trygdeavtale_in');
 const {lovvalgsbestemmelser_trygdeavtale_it} = require('./lovvalgsbestemmelser_trygdeavtale_it');
 const {lovvalgsbestemmelser_trygdeavtale_me} = require('./lovvalgsbestemmelser_trygdeavtale_me');
 const {lovvalgsbestemmelser_trygdeavtale_pt} = require('./lovvalgsbestemmelser_trygdeavtale_pt');
 const {lovvalgsbestemmelser_trygdeavtale_rs} = require('./lovvalgsbestemmelser_trygdeavtale_rs');
+const {lovvalgsbestemmelser_trygdeavtale_si} = require('./lovvalgsbestemmelser_trygdeavtale_si');
 const {lovvalgsbestemmelser_trygdeavtale_tr} = require('./lovvalgsbestemmelser_trygdeavtale_tr');
 const {lovvalgsbestemmelser_trygdeavtale_us} = require('./lovvalgsbestemmelser_trygdeavtale_us');
 const {tilleggsbestemmelser_trygdeavtale_ca} = require('./tilleggsbestemmelser_trygdeavtale_ca');
@@ -28,12 +30,14 @@ const trygdeavtale = {
   lovvalgsbestemmelser_trygdeavtale_fr,
   lovvalgsbestemmelser_trygdeavtale_gb,
   lovvalgsbestemmelser_trygdeavtale_gr,
+  lovvalgsbestemmelser_trygdeavtale_hr,
   lovvalgsbestemmelser_trygdeavtale_il,
   lovvalgsbestemmelser_trygdeavtale_in,
   lovvalgsbestemmelser_trygdeavtale_it,
   lovvalgsbestemmelser_trygdeavtale_me,
   lovvalgsbestemmelser_trygdeavtale_pt,
   lovvalgsbestemmelser_trygdeavtale_rs,
+  lovvalgsbestemmelser_trygdeavtale_si,
   lovvalgsbestemmelser_trygdeavtale_tr,
   lovvalgsbestemmelser_trygdeavtale_us,
   tilleggsbestemmelser_trygdeavtale_ca,
