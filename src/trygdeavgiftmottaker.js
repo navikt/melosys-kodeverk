78cb54067f7d68466d887dec336abdcd42cf96d6
Janita Johansen <janita.marielle.johansen@nav.no>
2023-04-14 08:56:29 +0200

    Nye kodeverk: skatteplikttype og trygdeavgiftmottaker.js

---
+++ src/trygdeavgiftmottaker.js
@@ -0,0 +1,16 @@
+/**
+ * Kodeverk/trygdeavgiftMottaker
+ * @module
+ */
+const trygdeavgiftmottaker = [
+  {
+    kode: 'TRYGDEAVGIFT_BETALES_TIL_SKATT',
+    term: 'Trygdeavgift skal betales til Skatteetaten'
+  },
+  {
+    kode: 'TRYGDEAVGIFT_BETALES_TIL_NAV',
+    term: 'Trygdeavgift skal betales til Nav',
+  }
+];
+
+module.exports.trygdeavgiftmottaker = trygdeavgiftmottaker;
