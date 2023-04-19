e7f92d05b51c562eea90eb376450ed1b9998a288
Francois Allix <32903762+frans-a@users.noreply.github.com>
2023-04-19 11:54:36 +0200

    Avgiftsdekning
    Til beregning av trygdeavgift

---
+++ src/avgiftsdekning.js
@@ -0,0 +1,19 @@
+const avgiftsdekning = [
+  {
+    kode: 'HELSEDEL_UTEN_SYKEPENGER',
+    term: 'Helsedel uten sykepenger',
+  },
+  {
+    kode: 'HELSEDEL_MED_SYKEPENGER',
+    term: 'Helsedel med sykepenger',
+  },
+  {
+    kode: 'PENSJONSDEL_UTEN_YRKESSKADETRYGD',
+    term: 'Pensjonsdel uten yrkesskadetrygd',
+  },
+  {
+    kode: 'PENSJONSDEL_MED_YRKESSKADETRYGD',
+    term: 'Pensjonsdel med yrkesskadetrygd',
+  },
+];
+module.exports.avgiftsdekning = avgiftsdekning;
