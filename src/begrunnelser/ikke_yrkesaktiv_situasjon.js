7fdbfdb4f427097c5b6707168c66beea5fa149c8
Eirik Bergande <eirik.bergande@nav.no>
2023-05-03 14:07:10 +0200

    Legger på kodeverk for ikke yrkesaktiv

---
+++ src/begrunnelser/ikke_yrkesaktiv_situasjon.js
@@ -0,0 +1,19 @@
+const ikke_yrkesaktiv_situasjon = [
+  {
+    'kode': 'STUDENT',
+    'term': 'Student'
+  },
+  {
+    'kode': 'PRAKTIKANT_UTEN_LØNN',
+    'term': 'Praktikant uten ordinær lønn'
+  },
+  {
+    'kode': 'MEDFØLGENDE_FAMILIEMEDLEM',
+    'term': 'Medfølgende familiemedlem'
+  },
+  {
+    'kode': 'ANNET',
+    'term': 'Andre som ikke er i ordinært arbeid'
+  }
+];
+module.exports.ikke_yrkesaktiv_situasjon = ikke_yrkesaktiv_situasjon;
