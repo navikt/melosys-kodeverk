ef392d4b74e5d1fc8ac7c20dd09cf1b15bdf3cd3
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2023-12-07 11:37:25 +0100

    6127 Ny oppgavetype for manglende innbetaling

--- src/oppgavetyper.js
+++ src/oppgavetyper.js
@@ -3,29 +3,33 @@
  * @module
  */
 const oppgavetyper = [
-  {
-    kode: 'BEH_SAK',
-    term: 'Behandle sak'
-  },
-  {
-    kode: 'BEH_SAK_MK',
-    term: 'Behandle sak (Manuell)'
-  },
-  {
-    kode: 'JFR',
-    term: 'Journalføring'
-  },
-  {
-    kode: 'VUR',
-    term: 'Vurder dokument'
-  },
-  {
-    kode: 'VURD_HENV',
-    term: 'Vurder henvendelse'
-  },
-  {
-    kode: 'BEH_SED',
-    term: 'Behandle SED'
-  }
+    {
+        kode: 'BEH_SAK',
+        term: 'Behandle sak'
+    },
+    {
+        kode: 'BEH_SAK_MK',
+        term: 'Behandle sak (Manuell)'
+    },
+    {
+        kode: 'JFR',
+        term: 'Journalføring'
+    },
+    {
+        kode: 'VUR',
+        term: 'Vurder dokument'
+    },
+    {
+        kode: 'VURD_HENV',
+        term: 'Vurder henvendelse'
+    },
+    {
+        kode: 'BEH_SED',
+        term: 'Behandle SED'
+    },
+    {
+        kode: 'VURD_MAN_INNB',
+        term: 'Vurder manglende innbetaling'
+    }
 ];
 module.exports.oppgavetyper = oppgavetyper;
