ae7fc75727da7e1040f6cd5766beb402f90a5005
Marton Skjæveland <marton.skjeveland@nav.no>
2024-01-25 13:11:11 +0100

    Lagt til koder for ftrl 2.7 begrunnelser

---
+++ src/begrunnelser/folketrygdloven/ftrl_2_7_begrunnelser.js
@@ -0,0 +1,20 @@
+const ftrl_2_7_begrunnelser = [
+  {
+    kode: 'AMBASSADEPERSONELL',
+    term: 'Ambassadepersonell',
+  },
+  {
+    kode: 'ANSATT_MELLOMFOLKELIG_ORGANISASJON',
+    term: 'Ansatt i mellomfolkelig organisasjon',
+  },
+  {
+    kode: 'SELVSTENDIG_NÆRINGSDRIVENDE',
+    term: 'Selvstendig næringsdrivende bosatt i utlandet',
+  },
+  {
+    kode: 'ANNEN_GRUNN',
+    term: 'Annen grunn (fritekst)',
+  }
+];
+
+module.exports.ftrl_2_7_begrunnelser = ftrl_2_7_begrunnelser;
