441d3f95b485fce50aa8d5afeaa2e3b4a31a355f
Isa Salem Hame <isa.salem.hame@nav.no>
2024-01-31 12:07:26 +0100

    6368: Nye vilkår for § 2.7 og § 2.8 (#368)
    https://jira.adeo.no/browse/MELOSYS-6368

--- src/vilkaar/vilkaar.js
+++ src/vilkaar/vilkaar.js
@@ -62,6 +62,26 @@ const vilkaar = [
   {
     kode: 'FTRL_2_8_NÆR_TILKNYTNING_NORGE',
     term: 'Har søker nær tilknytning til det norske samfunnet?'
+  },
+  {
+    kode: 'FTRL_2_1A_TRYGDEKOORDINGERING',
+    term: 'Har du kommet frem til at søker ikke er omfattet av et annet lands trygdelovgivning, etter en avtale som hindrer frivillig medlemskap?'
+  },
+  {
+    kode: 'FTRL_2_7_IKKE_PLIKTIG_MEDLEM',
+    term: 'Oppholder søker seg i Norge uten å være pliktig medlem?'
+  },
+  {
+    kode: 'FTRL_2_7_RIMELIGHETSVURDERING',
+    term: 'Er det, etter en samlet vurdering, rimelig at søker får innvilget frivillig medlemskap?'
+  },
+  {
+    kode: 'FTRL_2_7A_BOSATT_I_NORGE',
+    term: 'Er søker bosatt i Norge?'
+  },
+  {
+    kode: 'FTRL_2_7A_SKIP_UTENFOR_EØS',
+    term: 'Er søker i arbeid på et skip registrert i en stat utenfor EØS-området?'
   }
 ];
 module.exports.vilkaar = vilkaar;
