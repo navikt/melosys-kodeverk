d398fddfe124b7ffe7c5c16dde3a334485818aab
Francois Allix <32903762+frans-a@users.noreply.github.com>
2024-02-15 13:56:01 +0100

    ftrl avklarte fakta ikke yrkesaktive

---
+++ src/ikkeyrkesaktivoppholdtype.js
@@ -0,0 +1,11 @@
+const ikkeyrkesaktivoppholdtype = [
+  {
+    kode: 'MIDLERTIDIG_2_1_FJERDE_LEDD',
+    term: 'Midlertidig opphold i utlandet',
+  },
+  {
+    kode: 'VEKSELVIS_2_1_FJERDE_LEDD',
+    term: 'Vekselsvis opphold i utlandet og Norge',
+  },
+];
+module.exports.ikkeyrkesaktivoppholdtype = ikkeyrkesaktivoppholdtype;
