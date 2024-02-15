feb67d425b79599f56f54302cbbe2c3ace7009f1
Francois <francois.allix@nav.no>
2024-02-15 15:37:45 +0100

    6331 Ftrl avklarte fakta ikke yrkesaktive (#373)
    https://jira.adeo.no/browse/MELOSYS-6331
    
    
    https://confluence.adeo.no/display/TEESSI/Spesifikke+kodeverk+FTRL#SpesifikkekodeverkFTRL-IkkeYrkesaktivOppholdType

---
+++ src/ikkeyrkesaktivrelasjontype.js
@@ -0,0 +1,23 @@
+const ikkeyrkesaktivrelasjontype = [
+  {
+    kode: 'BARN_2_5_ANDRE_LEDD',
+    term: 'Barn',
+  },
+  {
+    kode: 'EKTEFELLE_2_5_ANDRE_LEDD_A_TIL_B',
+    term: 'Ektefelle til en person nevnt i § 2-5 bokstav a til b ',
+  },
+  {
+    kode: 'EKTEFELLE_2_5_ANDRE_LEDD_C_TIL_E',
+    term: 'Ektefelle til person nevnt i § 2-5 bokstav c til e',
+  },
+  {
+    kode: 'BARN_2_8_FJERDE_LEDD',
+    term: 'Barn',
+  },
+  {
+    kode: 'EKTEFELLE_2_8_FJERDE_LEDD',
+    term: 'Ektefelle',
+  },
+];
+module.exports.ikkeyrkesaktivrelasjontype = ikkeyrkesaktivrelasjontype;
