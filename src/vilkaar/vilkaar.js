327488a907fae444a7d5df4f479649f8fe80460f
Francois <francois.allix@nav.no>
2024-02-19 14:26:09 +0100

    6331 Ftrl vilkår (#374)
    https://jira.adeo.no/browse/MELOSYS-6331

--- src/vilkaar/vilkaar.js
+++ src/vilkaar/vilkaar.js
@@ -56,7 +56,71 @@ const vilkaar = [
     term: null,
   },
   {
+    kode: 'FTRL_2_1A_TRYGDEKOORDINGERING',
+    term: 'Har du kommet frem til at søker ikke er omfattet av et annet lands trygdelovgivning, etter en avtale som hindrer frivillig medlemskap?'
+  },
+  {
+    kode: 'FTRL_2_1_BOSATT_NORGE',
+    term: 'Har søker oppholdt seg eller hatt til hensikt å oppholde seg i Norge i minst 12 måneder?'
+  },
+  {
+    kode: 'FTRL_2_1_BOSATT_NORGE_FORUT',
+    term: 'Har søker oppholdt seg eller hatt til hensikt å oppholde seg i Norge i minst 12 måneder forut for utenlandsoppholdet?'
+  },
+  {
+    kode: 'FTRL_2_1_LOVLIG_OPPHOLD',
+    term: 'Har søker lovlig opphold i Norge?'
+  },
+  {
+    kode: 'FTRL_2_1_OPPHOLD_UNDER_12MND',
+    term: 'Er søkers utenlandsopphold ment å vare i under 12 måneder? Eller mindre enn seks måneder per kalenderår, over to eller flere påfølgende kalenderår?'
+  },
+  {
+    kode: 'FTRL_2_5_NORSK_STATSBORGER_EØS_BORGER',
+    term: 'Er søker norsk statsborger/EØS-borger?'
+  },
+  {
+    kode: 'FTRL_2_5_LÅN_STIPEND_LÅNEKASSEN',
+    term: 'Mottar søker lån og/eller stipend fra Lånekassen?'
+  },
+  {
+    kode: 'FTRL_2_5_MEDFØLGENDE_A_E',
+    term: 'Er søker medfølgende til en person som har fått innvilget medlemskap etter § 2-5 bokstav a til e?'
+  },
+  {
+    kode: 'FTRL_2_5_FORSØRGET_FAMILIEMEDLEM',
+    term: 'Er søker forsørget og oppholder seg sammen med denne personen?'
+  },
+  {
+    kode: 'FTRL_2_7_IKKE_PLIKTIG_MEDLEM',
+    term: 'Oppholder søker seg i Norge uten å være pliktig medlem?'
+  },
+  {
+    kode: 'FTRL_2_7_RIMELIGHETSVURDERING',
+    term: 'Er det, etter en samlet vurdering, rimelig at søker får innvilget frivillig medlemskap?'
+  },
+  {
+    kode: 'FTRL_2_7_FORSØRGET_FAMILIEMEDLEM',
+    term: 'Er søker forsørget og oppholder seg sammen med en person som er medlem etter § 2-7 første ledd?'
+  },
+  {
+    kode: 'FTRL_2_7_INGEN_SÆRLIGE_GRUNNER_TALER_IMOT',
+    term: 'Har du kommet frem til at det ikke er særlige grunner som taler imot at søker skal få innvilget frivillig medlemskap?'
+  },
+  {
+    kode: 'FTRL_2_7A_BOSATT_I_NORGE',
+    term: 'Er søker bosatt i Norge?'
+  },
+  {
+    kode: 'FTRL_2_7A_SKIP_UTENFOR_EØS',
+    term: 'Er søker i arbeid på et skip registrert i en stat utenfor EØS-området?'
+  },
+  {
     kode: 'FTRL_2_8_FORUTGÅENDE_TRYGDETID',
+    term: 'Har søker vært medlem i minst tre av de fem siste kalenderårene?'
+  },
+  {
+    kode: 'FTRL_FORUTGÅENDE_TRYGDETID',
     term: 'Har søker vært medlem i minst tre av de fem siste kalenderårene?'
   },
   {
@@ -68,24 +132,12 @@ const vilkaar = [
     term: 'Har søker nær tilknytning til det norske samfunnet?'
   },
   {
-    kode: 'FTRL_2_1A_TRYGDEKOORDINGERING',
-    term: 'Har du kommet frem til at søker ikke er omfattet av et annet lands trygdelovgivning, etter en avtale som hindrer frivillig medlemskap?'
+    kode: 'FTRL_2_8_STUDENT_UVIVERSITET_HØGSKOLE',
+    term: 'Er søker student ved universitet eller høgskole?'
   },
   {
-    kode: 'FTRL_2_7_IKKE_PLIKTIG_MEDLEM',
-    term: 'Oppholder søker seg i Norge uten å være pliktig medlem?'
-  },
-  {
-    kode: 'FTRL_2_7_RIMELIGHETSVURDERING',
-    term: 'Er det, etter en samlet vurdering, rimelig at søker får innvilget frivillig medlemskap?'
-  },
-  {
-    kode: 'FTRL_2_7A_BOSATT_I_NORGE',
-    term: 'Er søker bosatt i Norge?'
-  },
-  {
-    kode: 'FTRL_2_7A_SKIP_UTENFOR_EØS',
-    term: 'Er søker i arbeid på et skip registrert i en stat utenfor EØS-området?'
+    kode: 'FTRL_2_8_FORSØRGET_FAMILIEMEDLEM',
+    term: 'Er søker forsørget og oppholder seg sammen med en person som er medlem etter § 2-8 første eller andre ledd?'
   }
 ];
 module.exports.vilkaar = vilkaar;
