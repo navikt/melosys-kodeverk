c2ec9337b095b15a1a7c0423cddd6631077a637d
barstadEkstern <oystein.madsen.barstad@nav.no>
2024-03-07 14:22:30 +0100

    Nye inntektskildetyper

--- src/inntektskildetype.js
+++ src/inntektskildetype.js
@@ -1,31 +1,49 @@
 const inntektskildetype = [
-  {
-    kode: 'ARBEIDSINNTEKT_FRA_NORGE',
-    term: 'Arbeidsinntekt fra Norge',
-  },
-  {
-    kode: 'NÆRINGSINNTEKT_FRA_NORGE',
-    term: 'Næringsinntekt fra Norge',
-  },
-  {
-    kode: 'INNTEKT_FRA_UTLANDET',
-    term: 'Inntekt fra utlandet',
-  },
-  {
-    kode: 'FN_SKATTEFRITAK',
-    term: 'Ansatt i FN med skattefritak',
-  },
-  {
-    kode: 'MISJONÆR',
-    term: 'Misjonær som skal arbeide i utlandet i minst to år',
-  },
-  {
-    kode: 'PENSJON_UFØRETRYGD',
-    term: 'Pensjon/uføretrygd',
-  },
-  {
-    kode: 'PENSJON_UFØRETRYGD_KILDESKATT',
-    term: 'Pensjon/uføretrygd det betales kildeskatt av',
-  },
+    {
+        kode: 'ARBEIDSINNTEKT_FRA_NORGE',
+        term: 'Arbeidsinntekt fra Norge',
+    },
+    {
+        kode: 'NÆRINGSINNTEKT_FRA_NORGE',
+        term: 'Næringsinntekt fra Norge',
+    },
+    {
+        kode: 'INNTEKT_FRA_UTLANDET',
+        term: 'Inntekt fra utlandet',
+    },
+    {
+        kode: 'FN_SKATTEFRITAK',
+        term: 'Ansatt i FN med skattefritak',
+    },
+    {
+        kode: 'MISJONÆR',
+        term: 'Misjonær som skal arbeide i utlandet i minst to år',
+    },
+    {
+        kode: 'PENSJON_UFØRETRYGD',
+        term: 'Pensjon/uføretrygd',
+    },
+    {
+        kode: 'PENSJON_UFØRETRYGD_KILDESKATT',
+        term: 'Pensjon/uføretrygd det betales kildeskatt av',
+    },
+    {
+        kode: 'ARBEIDSINNTEKT',
+        term: 'Arbeidsinntekt fra norsk eller utenlandsk virksomhet. ' +
+            'Forutsetning om at norsk arbeidsgiver alltid innraporterer arbeidsgiveravift  til norske myndigheter',
+    },
+    {
+        kode: 'NÆRINGSINNTEKT',
+        term: 'Næringsinntekt fra norsk eller utenlandsk virksomhet. ' +
+            'Dvs. inntekt fra enkeltmannsforetak der det ikke betales arbeidsgiveravgift.',
+    },
+    {
+        kode: 'PENSJON',
+        term: 'Pensjon fra Norge eller utlandet',
+    },
+    {
+        kode: 'UFØRETRYGD',
+        term: 'Inntekt fra uføretrygd',
+    },
 ];
 module.exports.inntektskildetype = inntektskildetype;
