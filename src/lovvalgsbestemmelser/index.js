94c12b4eb097d61ed598bbcf41e86f0f3c0db486
Janita Johansen <56385061+janitamarielle@users.noreply.github.com>
2024-04-19 14:32:59 +0200

    6566 Nye lovvalgsbestemmelser (#391)
    https://jira.adeo.no/browse/MELOSYS-6566
    
    
    https://confluence.adeo.no/display/TEESSI/Spesifikk+kodeverk+Konv_EFTA_Storbritannia

--- src/lovvalgsbestemmelser/index.js
+++ src/lovvalgsbestemmelser/index.js
@@ -2,6 +2,7 @@ const {lovvalgbestemmelser_883_2004} = require('./lovvalgbestemmelser_883_2004')
 const {lovvalgbestemmelser_987_2009} = require('./lovvalgbestemmelser_987_2009');
 const {tilleggsbestemmelser_883_2004} = require('./tilleggsbestemmelser_883_2004');
 const {overgangsregelbestemmelser} = require('./overgangsregelbestemmelser');
+const {lovvalgbestemmelser_konv_efta_storbritannia} = require('./lovvalgbestemmelser_konv_efta_storbritannia');
 const {trygdeavtale} = require('./trygdeavtale');
 
 const lovvalgsbestemmelser = {
@@ -9,6 +10,7 @@ const lovvalgsbestemmelser = {
   lovvalgbestemmelser_987_2009,
   tilleggsbestemmelser_883_2004,
   overgangsregelbestemmelser,
+  lovvalgbestemmelser_konv_efta_storbritannia,
   trygdeavtale,
 };
 module.exports.lovvalgsbestemmelser = lovvalgsbestemmelser;
---
+++ src/lovvalgsbestemmelser/lovvalgbestemmelser_konv_efta_storbritannia.js
@@ -0,0 +1,73 @@
+const lovvalgbestemmelser_konv_efta_storbritannia = [
+  {
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_3A',
+    term: 'Konv. EFTA Storbritannia art.13(3)(a)',
+  }, {
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_3B',
+    term: 'Konv. EFTA Storbritannia art.13(3)(b)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_3C',
+    term: 'Konv. EFTA Storbritannia art.13(3)(c)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_3D',
+    term: 'Konv. EFTA Storbritannia art.13(3)(d)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_4',
+    term: 'Konv. EFTA Storbritannia art.13(4)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART13_4_2',
+    term: 'Konv. EFTA Storbritannia art.13(4)(2)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART14_1',
+    term: 'Konv. EFTA Storbritannia art.14(1)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART14_2',
+    term: 'Konv. EFTA Storbritannia art.14(2)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_1A',
+    term: 'Konv. EFTA Storbritannia art.15(1)(a)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_1B1',
+    term: 'Konv. EFTA Storbritannia art.15(1)(b)(i)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_1_B2',
+    term: 'Konv. EFTA Storbritannia art.15(1)(b)(ii)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_1_B3',
+    term: 'Konv. EFTA Storbritannia art.15(1)(b)(iii)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_1_B4',
+    term: 'Konv. EFTA Storbritannia art.15(1)(b)(iv)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_2A',
+    term: 'Konv. EFTA Storbritannia art.15(2)(a)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_2B',
+    term: 'Konv. EFTA Storbritannia art.15(2)(b)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_3',
+    term: 'Konv. EFTA Storbritannia art.15(3)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_4',
+    term: 'Konv. EFTA Storbritannia art.15(4)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART15_5',
+    term: 'Konv. EFTA Storbritannia art.15(5)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART16_1',
+    term: 'Konv. EFTA Storbritannia art.16(1)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART16_3',
+    term: 'Konv. EFTA Storbritannia art.16(3)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART16_4',
+    term: 'Konv. EFTA Storbritannia art.16(4)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART18_1',
+    term: 'Konv. EFTA Storbritannia art.18(1)',
+  },{
+    kode: 'KONV_EFTA_STORBRITANNIA_ART18_2',
+    term: 'Konv. EFTA Storbritannia art.18(2)',
+  },
+];
+module.exports.lovvalgbestemmelser_konv_efta_storbritannia = lovvalgbestemmelser_konv_efta_storbritannia;
