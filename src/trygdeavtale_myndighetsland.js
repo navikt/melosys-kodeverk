0093eb7b44b29802ff9b7289c102d548227d8665
Fredrik Olsen Hauge <45845087+fredrik-oh@users.noreply.github.com>
2024-06-03 15:42:59 +0200

    6646 Legger til Korea i trygdeavtale_myndighetsland.js (#399)
    https://jira.adeo.no/browse/MELOSYS-6646
    
    Co-authored-by: Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>

--- src/trygdeavtale_myndighetsland.js
+++ src/trygdeavtale_myndighetsland.js
@@ -3,81 +3,85 @@
  * @module
  */
 const trygdeavtale_myndighetsland = [
-  {
-    kode: 'AU',
-    term: 'Australia',
-  },
-  {
-    kode: 'BA',
-    term: 'Bosnia-Hercegovina',
-  },
-  {
-    kode: 'CA',
-    term: 'Canada',
-  },
-  {
-    kode: 'CL',
-    term: 'Chile',
-  },
-  {
-    kode: 'FR',
-    term: 'Frankrike',
-  },
-  {
-    kode: 'GR',
-    term: 'Hellas',
-  },
-  {
-    kode: 'IN',
-    term: 'India',
-  },
-  {
-    kode: 'IL',
-    term: 'Israel',
-  },
-  {
-    kode: 'IT',
-    term: 'Italia',
-  },
-  {
-    kode: 'HR',
-    term: 'Kroatia',
-  },
-  {
-    kode: 'ME',
-    term: 'Montenegro',
-  },
-  {
-    kode: 'PT',
-    term: 'Portugal',
-  },
-  {
-    kode: 'CA_QC',
-    term: 'Quebec',
-  },
-  {
-    kode: 'RS',
-    term: 'Serbia',
-  },
-  {
-    kode: 'SI',
-    term: 'Slovenia',
-  },
-  {
-    kode: 'GB',
-    term: 'Storbritannia',
-  },
-  {
-    kode: 'CH',
-    term: 'Sveits',
-  },
-  {
-    kode: 'TR',
-    term: 'Tyrkia',
-  },
-  {
-    kode: 'US',
-    term: 'USA',
-  }
+    {
+        kode: 'AU',
+        term: 'Australia',
+    },
+    {
+        kode: 'BA',
+        term: 'Bosnia-Hercegovina',
+    },
+    {
+        kode: 'CA',
+        term: 'Canada',
+    },
+    {
+        kode: 'CL',
+        term: 'Chile',
+    },
+    {
+        kode: 'FR',
+        term: 'Frankrike',
+    },
+    {
+        kode: 'GR',
+        term: 'Hellas',
+    },
+    {
+        kode: 'IN',
+        term: 'India',
+    },
+    {
+        kode: 'IL',
+        term: 'Israel',
+    },
+    {
+        kode: 'IT',
+        term: 'Italia',
+    },
+    {
+        kode: 'HR',
+        term: 'Kroatia',
+    },
+    {
+        kode: 'ME',
+        term: 'Montenegro',
+    },
+    {
+        kode: 'PT',
+        term: 'Portugal',
+    },
+    {
+        kode: 'CA_QC',
+        term: 'Quebec',
+    },
+    {
+        kode: 'RS',
+        term: 'Serbia',
+    },
+    {
+        kode: 'SI',
+        term: 'Slovenia',
+    },
+    {
+        kode: 'GB',
+        term: 'Storbritannia',
+    },
+    {
+        kode: 'CH',
+        term: 'Sveits',
+    },
+    {
+        kode: 'TR',
+        term: 'Tyrkia',
+    },
+    {
+        kode: 'US',
+        term: 'USA',
+    },
+    {
+        kode: 'KR',
+        term: 'Korea',
+    }
 ];
 module.exports.trygdeavtale_myndighetsland = trygdeavtale_myndighetsland;
