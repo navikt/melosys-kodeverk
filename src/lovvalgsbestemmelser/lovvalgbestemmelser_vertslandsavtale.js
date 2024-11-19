106becb3a3c6ec18651953b96093b305d806cec6
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2024-11-19 13:55:28 +0100

    fix?

--- src/lovvalgsbestemmelser/lovvalgbestemmelser_vertslandsavtale.js
+++ src/lovvalgsbestemmelser/lovvalgbestemmelser_vertslandsavtale.js
@@ -1,16 +1,16 @@
 const lovvalgbestemmelser_vertslandsavtale = [
     {
         kode: 'ARKTISK_RÅDS_SEKRETARIAT_ART16',
-        term: 'Arktisk råds sekretariat - art. 16'
+        term: 'Arktisk råds sekretariat - art. 16',
     }, {
         kode: 'DET_INTERNASJONALE_ BARENTSSEKRETARIATET_ART14',
-        term: 'Det internasjonale Barentssekretariatet - art. 14'
+        term: 'Det internasjonale Barentssekretariatet - art. 14',
     }, {
         kode: 'DEN_NORDATLANTISKE_SJØPATTEDYRKOMMISJON_ART16',
-        term: 'Den nordatlantiske sjøpattedyrkommisjon - art. 16'
+        term: 'Den nordatlantiske sjøpattedyrkommisjon - art. 16',
     }, {
         kode: 'TILLEGGSAVTALE_NATO',
-        term: 'Tilleggsavtale med NATO av 22.04.1988'
+        term: 'Tilleggsavtale med NATO av 22.04.1988',
     },
 ];
 module.exports.lovvalgbestemmelser_vertslandsavtale = lovvalgbestemmelser_vertslandsavtale;
