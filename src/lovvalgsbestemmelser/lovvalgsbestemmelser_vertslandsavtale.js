caba39346a8e6247fd537a64a1b667cde9f0b7aa
Fredrik Olsen Hauge <fredrik.olsen.hauge@nav.no>
2024-11-19 11:44:12 +0100

    Fikset en feil i lovvalgsbestemmelser_vertslandsavtale.js

--- src/lovvalgsbestemmelser/lovvalgsbestemmelser_vertslandsavtale.js
+++ src/lovvalgsbestemmelser/lovvalgsbestemmelser_vertslandsavtale.js
@@ -1,6 +1,6 @@
 const lovvalgbestemmelser_vertslandsavtale = [
     {
-        kode: 'ARKTISK_RÅDS_SEKRETARIAT_ART16_STABSMEDLEM',
+        kode: 'ARKTISK_RÅDS_SEKRETARIAT_ART16',
         term: 'Arktisk råds sekretariat - art. 16',
     }, {
         kode: 'DET_INTERNASJONALE_ BARENTSSEKRETARIATET_ART14',
