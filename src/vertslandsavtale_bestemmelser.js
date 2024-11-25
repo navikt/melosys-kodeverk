b4eb5998db06d367859453622347b87b8344159c
Eirik Bergande <eirik.bergande@nav.no>
2024-11-25 15:30:59 +0100

    endret fra lovvalg til bestemmelse

---
+++ src/vertslandsavtale_bestemmelser.js
@@ -0,0 +1,16 @@
+const vertslandsavtale_bestemmelser = [
+  {
+    kode: 'ARKTISK_RÅDS_SEKRETARIAT_ART16',
+    term: 'Arktisk råds sekretariat - art. 16',
+  }, {
+    kode: 'DET_INTERNASJONALE_BARENTSSEKRETARIATET_ART14',
+    term: 'Det internasjonale Barentssekretariatet - art. 14',
+  }, {
+    kode: 'DEN_NORDATLANTISKE_SJØPATTEDYRKOMMISJON_ART16',
+    term: 'Den nordatlantiske sjøpattedyrkommisjon - art. 16',
+  }, {
+    kode: 'TILLEGGSAVTALE_NATO',
+    term: 'Tilleggsavtale med NATO av 22.04.1988',
+  },
+];
+module.exports.vertslandsavtale_bestemmelser = vertslandsavtale_bestemmelser;
