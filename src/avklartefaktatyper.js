71b3596d20e0dfd7e79a7237275380edc4ffcb6c
Isa Salem Hame <isa.salem.hame@nav.no>
2025-01-06 13:57:31 +0100

    MELOSYS-7012: Legg til ukjent sluttdato som avklart fakta

--- src/avklartefaktatyper.js
+++ src/avklartefaktatyper.js
@@ -78,6 +78,10 @@ const avklartefaktatyper = [
   {
     kode: 'ARBEIDSSITUASJON',
     term: null
+  },
+  {
+    kode: 'UKJENT_SLUTTDATO',
+    term: null
   }
 ];
 module.exports.avklartefaktatyper = avklartefaktatyper;
