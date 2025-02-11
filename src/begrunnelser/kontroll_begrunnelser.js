3038c866edaddf256342b431cf27f06ba2e4645f
Thang Phan <thang.quoc.ly.phan@nav.no>
2025-02-11 11:53:59 +0100

    6850 Vertslandsavtalen trygdeavgift, MANGLENDE_FULLMEKTIG_MEDLEM_ETTER_VERTSLANDSAVTALE (#424)

--- src/begrunnelser/kontroll_begrunnelser.js
+++ src/begrunnelser/kontroll_begrunnelser.js
@@ -177,6 +177,10 @@ const kontroll_begrunnelser = [
   {
     kode: 'OVERLAPPENDE_PERIODE_MED_FORSKUDDSVIS_FAKTURERUNG',
     term: 'Det finnes allerede en sak med overlappende medlemskapsperiode og beregnet avgift. Vurder om det er riktig at bruker skal ha to overlappende saker, eller om du heller skal gjøre en ny vurdering av den andre saken'
+  },
+  {
+    kode: 'MANGLENDE_FULLMEKTIG_MEDLEM_ETTER_VERTSLANDSAVTALE',
+    term: 'I følge vertslandsavtalen skal arbeidsgiver sørge for at trygdeavgift innbetales. Vurder om arbeidsgiver skal registreres som fullmektig for trygdeavgift'
   }
 ];
 module.exports.kontroll_begrunnelser = kontroll_begrunnelser;
